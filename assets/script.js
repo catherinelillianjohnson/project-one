5b0b31279fe8843232506c81e11ba69b4c9c31e9
Tyler Smith <tylersmith2614@gmail.com>
2023-04-03 19:05:57 -0600

    console logging ingredients

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,37 @@
 var spoontacularAPI = "apiKey=8f6102895a99438d990df314e0de78d0";
 var rootURL = "https://api.spoonacular.com/recipes/complexSearch?";
-
 var searchBtn = $("search-btn");
 var mealList = $("meal");
 
 
 
 function search(food) {
-  fetch(rootURL + spoontacularAPI + "&query=" + food)
+  fetch(rootURL + spoontacularAPI + "&query=" + food + "&fillIngredients=true")
   .then(function(response){
     return response.json();
   }) 
   .then(function(data) {
-    console.log(data.results);
+    $("")
+    console.log(data.results[0].title);
+    console.log(data.results[0].missedIngredients[0].name)
+    console.log(data.results[0].missedIngredients[1].name)
+    console.log(data.results[0].missedIngredients[2].name)
+    console.log(data.results[0].missedIngredients[3].name)
+    console.log(data.results[0].missedIngredients[4].name)
+    console.log(data.results[0].missedIngredients[5].name)
+    console.log(data.results[0].missedIngredients[6].name)
+    // create a for loop to create the ingredients list.
+    $("#search-history").append(data.results)
   })
 }
+
+
 
 $("#search-btn").on("click", function(event) {
   event.preventDefault();
   // .trim takes out the white spaces like if you add a space when searching
   var food = $("#search").val().trim();
   search(food)
-  console.log(food)
   $("#search").val("")
 })
 
@@ -30,18 +40,18 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("search-btn");
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-    modal.style.display = "block";
+// btn.onclick = function() {
+//     modal.style.display = "block";
   
-}
+// }
 
-span.onclick = function() {
-    modal.style.display = "None";
-}
+// span.onclick = function() {
+//     modal.style.display = "None";
+// }
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
 
