9815d41c39b18d107acee0f3811a14870cf8a585
Ethantran2002 <e.tran1117@gmail.com>
2023-04-10 18:22:04 -0600

    buttons for saved recipe into local storage

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,15 @@
-var spoontacularAPI = "apiKey=6d62c079711f4445911e77ede85400b1";
-// spare API Key:  7474228d0aa440408c3a09da9a064abe
+var spoontacularAPI = "apiKey=7474228d0aa440408c3a09da9a064abe";
+// spare API Key: 6d62c079711f4445911e77ede85400b1 
 var rootURL = "https://api.spoonacular.com/recipes/complexSearch?";
 var searchBtn = $("#search-btn");
 var mealList = $("meal");
-
-
+var savedBtnSection = $("#history-button-section");
 
 var EdamamUrl ="https://api.edamam.com/api/food-database/v2/parser?"
 var app_id="app_id=7cf60948"
 var app_key="&app_key=7a0e4d80d09649c34f64dba432baa17e"
 
-var ingr = "chicken"
+var recipeTitle = ""
 
 
 function search(food) {
@@ -25,10 +24,22 @@ function search(food) {
   }) 
   .then(function(data) {
  
-    console.log(data.results)
-    
    var randomRecipe = data.results[Math.floor(Math.random() * data.results.length)];
-    
+
+   //local storage to save each search
+
+   var recipeTitle = randomRecipe.title
+
+   var savedIds = JSON.parse(localStorage.getItem(recipeTitle)) || []
+   savedIds.push(randomRecipe)
+
+   localStorage.setItem(recipeTitle,JSON.stringify(savedIds))
+  
+    var resultItem = document.createElement("button");
+    resultItem.setAttribute("id", "history-btn");
+    resultItem.setAttribute("display", "block");
+    resultItem.textContent = randomRecipe.title
+    $(".history-button-section").append(resultItem);
 
     $("#search-history").append(data.results);
 
@@ -36,9 +47,7 @@ function search(food) {
 
     for (let i = 0; i < randomRecipe.missedIngredients.length; i++) {
 
-
-
-      ingr = randomRecipe.missedIngredients[i].name
+      var ingr = randomRecipe.missedIngredients[i].name
     
       fetch (EdamamUrl + app_id + app_key + "&ingr=" + ingr)
       .then(function(response){
@@ -59,11 +68,6 @@ function search(food) {
         ingredientEl.appendChild(ingredientNameElement)
         ingredientEl.appendChild(caloriesEl)
         document.getElementById("ingredient-list").appendChild(ingredientEl)
-        
-        console.log(data.hints.measures)
-
-        
-
       })
       
     }
@@ -76,14 +80,93 @@ function search(food) {
     var recipePictureElement = document.createElement("img")
     recipePictureElement.src = randomRecipe.image
     var src = document.getElementById("recipeImg")
-    src.appendChild(recipePictureElement)
-  });
+    src.appendChild(recipePictureElement) })
 
-};
+  };
+
+//to create buttons for searches already in local storage
+function renderSavedItems() {
+
+  var pcstorage = localStorage
+
+  Object.keys(pcstorage).forEach(entry => {
+
+
+    var resultItem = document.createElement("button");
+    // resultItem.setAttribute("class", "history");
+    resultItem.setAttribute("id", "history-btn");
+    // resultItem.setAttribute("display", "block");
+    resultItem.textContent = entry
+    $(".history-button-section").append(resultItem);
+
+    console.log((entry))
+  })
+ }
+
+renderSavedItems()
+
+
+
+// function for the saved recipe buttons
+function reFetchRecipe(recipeName) {
+
+$("#recipeImg").empty()
+$("#ingredient-list").empty()
+$("#recipeTitle").empty()
+
+var localRecipe = JSON.parse(localStorage.getItem(recipeName)) || []
+
+randomRecipe = localRecipe[0]
+
+console.log(randomRecipe)
+
+for (let i = 0; i < randomRecipe.missedIngredients.length; i++) {
+
+  ingr = randomRecipe.missedIngredients[i].name
+
+  fetch (EdamamUrl + app_id + app_key + "&ingr=" + ingr)
+  .then(function(response){
+    return response.json();
+    
+  })
+  .then(function(data) {
+// append in ingredient picture, ingredient name, calorie count
+    var calories = data.hints[0].food.nutrients.ENERC_KCAL
+    var ingredientEl = document.createElement("li") 
+    var caloriesEl = document.createElement("p")
+    var ingredientPictureElement = document.createElement("img")
+    var ingredientNameElement = document.createElement("p")
+    ingredientNameElement.textContent = randomRecipe.missedIngredients[i].original
+    caloriesEl.textContent = calories + " calories"
+    ingredientPictureElement.src = randomRecipe.missedIngredients[i].image
+    ingredientEl.appendChild(ingredientPictureElement)
+    ingredientEl.appendChild(ingredientNameElement)
+    ingredientEl.appendChild(caloriesEl)
+    document.getElementById("ingredient-list").appendChild(ingredientEl)
+  })
+  
+}
+  // append in recipe title
+  var recipeTitleEl = document.getElementById("recipeTitle")
+  var recipeTitle = document.createTextNode(randomRecipe.title)
+  recipeTitleEl.appendChild(recipeTitle)
+
+  // append in image of recipe
+  var recipePictureElement = document.createElement("img")
+  recipePictureElement.src = randomRecipe.image
+  var src = document.getElementById("recipeImg")
+  src.appendChild(recipePictureElement) 
+  console.log("button works")
+}
+
+
 
 
 var arrayOfIngredients = ['ingredient1', 'ingredient2', 'ingredient3'];
 var randomIngredient = arrayOfIngredients[Math.floor(Math.random() * arrayOfIngredients.length)];
+
+
+
 
 searchBtn.on("click", function(event) {
   event.preventDefault();
@@ -93,21 +176,11 @@ searchBtn.on("click", function(event) {
   $("#search").val("");
 });
 
+savedBtnSection.on("click", function(event) {
+  event.preventDefault();
+  var recipeName = event.target.textContent
+  reFetchRecipe(recipeName);
+  $("#history").val("")
+  console.log("button works")
+});
 
-var card = document.getElementById("ingredientCard");
-var btn = document.getElementById("search-btn");
-var span = document.getElementsByClassName("close")[0];
-
-//  btn.onclick = function() {
-//      modal.style.display = "block";
- 
-//  }
-//  span.onclick = function() {
-//      modal.style.display = "None";
-//  }
-
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
